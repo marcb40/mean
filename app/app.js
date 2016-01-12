d4e2fe5dcdd2811b5b407a26ed0b3adccb6a72a9
Marc B <marcb40@gmail.com>
2016-01-12 10:39:37 -0500

    completed angular part

--- app/app.js
+++ app/app.js
@@ -12,7 +12,12 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
-    });
+    })
+    .state('posts', {
+	  url: '/posts/{id}',
+	  templateUrl: '/posts.html',
+	  controller: 'PostsCtrl'
+	});
 
   $urlRouterProvider.otherwise('home');
 }]);
@@ -21,18 +26,22 @@ app.controller('MainCtrl', [
 	'$scope',
 	'posts',
 	function($scope, posts){
-	  $scope.posts = posts.posts;
+	
 	  $scope.test = 'Hello world!';
-	  $scope.posts = [
-		  {title: 'post 1', upvotes: 5},
-		  {title: 'post 2', upvotes: 2},
-		  {title: 'post 3', upvotes: 15},
-		  {title: 'post 4', upvotes: 9},
-		  {title: 'post 5', upvotes: 4}
-		];
+	 
+
+		$scope.posts = posts.posts;
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === '') { return; }
-		 	$scope.posts.push({title: $scope.title,  link: $scope.link, upvotes: 0});
+		 	$scope.posts.push({
+		 		title: $scope.title,  
+		 		link: $scope.link, 
+		 		upvotes: 0,
+		 		comments: [
+				    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+				    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				  ]
+		 	});
 	  		$scope.title = '';
 	  		$scope.link = '';
 		};
@@ -46,6 +55,17 @@ app.controller('PostsCtrl', [
 	'$stateParams',
 	'posts',
 	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+
+		$scope.addComment = function(){
+		  if($scope.body === '') { return; }
+		  $scope.post.comments.push({
+		    body: $scope.body,
+		    author: 'user',
+		    upvotes: 0
+		  });
+		  $scope.body = '';
+		};
 
 }]);
 
